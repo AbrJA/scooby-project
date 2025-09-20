2a7b321386e2efedcfedea91d095b1ef0e6c8094
AbrJA <abraham.jaimes.mx@gmail.com>
2025-09-20 17:20:44 -0600

    Adding logging

--- indexer/src/indexer.py
+++ indexer/src/indexer.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import pandas as pd
 from PIL import Image
 
@@ -11,19 +12,17 @@ class Indexer:
     def run_indexing(self, dir_images, file_csv):
         # 1. Download files
         self.downloader.get_files()
-
         # 2. Set up Qdrant
         self.qdrant.create_client()
         self.qdrant.create_collection()
-
         # 3. Load embedder model
         self.embedder.load_model()
-
         # 4. Ingest data
         information = pd.read_csv(file_csv)
         image_files = [f for f in os.listdir(dir_images) if f.endswith('.jpg')]
 
         image_files = image_files[0:10]
+        total_images = len(image_files)
 
         for i, path in enumerate(image_files):
             image = Image.open(os.path.join(dir_images, path))
@@ -31,4 +30,5 @@ class Indexer:
             pet_id = os.path.splitext(os.path.basename(path))[0]
             payload = information[information['petid'] == pet_id].iloc[0].to_dict()
             self.qdrant.upsert_point(i, vector.flatten(), payload)
-            # Add logging here
+            if i % 100 == 0:
+                logging.info(f'Ingested {i}/{total_images} vectors into collection')
